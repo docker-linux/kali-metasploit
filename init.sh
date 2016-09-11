652c6e8a45dcd9c0082b4c88ec7a3273d6020da5
Pedro César <tecdom@users.noreply.github.com>
2016-09-11 18:39:30 -0300

    Update sources.list - Rolling
    Update sources.list with new repository of version rolling.

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # /etc/init.d/postgresql start
+echo deb http://http.kali.org/kali kali-rolling main contrib non-free >> /etc/apt/sources.list
 /usr/share/metasploit-framework/msfupdate
 /usr/share/metasploit-framework/msfconsole			# Throw us into the Metasploit console
